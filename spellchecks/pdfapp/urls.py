650a21e9c2f2397cf83633d97cc4bae9971cc609
Yash Patil <91176651+YashPatil07@users.noreply.github.com>
2025-01-08 16:03:54 +0530

    Delete spellchecks directory

--- spellchecks/pdfapp/urls.py
+++
@@ -1,9 +0,0 @@
-# pdfap/urls.py
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),  # Landing page / upload form
-    # path('annotated/', views.annotated_pdf, name='annotated_pdf'),  # Download link page
-]
